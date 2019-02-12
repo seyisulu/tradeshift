0194a5ca9af225b873b4489c3788782e082284a5
Emmanuel Olowosulu <eolowo@sulusoft.com>
2019-02-12 12:16:43 +0100

    Added Bosses and Root EIDs to Nodes
    Signed-off-by: Emmanuel Olowosulu <eolowo@sulusoft.com>

--- api/utils.js
+++ api/utils.js
@@ -7,17 +7,36 @@ function getId ({ identity }) {
 }
 
 function extract ({ records }) {
+  let root = '';
   const [ data ] = records;
   const rawNodes = get(data, 'nodes');
   const rawEdges = get(data, 'paths');
-  const nodes = rawNodes.map(n => ({
-    id: getId(n),
-    eid: n.properties.eid,
-    label: n.properties.name,
+  // Maps node IDs to their EID
+  const nodeMap = rawNodes.reduce((acc, node) => {
+    if (node.properties.name === 'Root') {
+      root = node.properties.eid;
+    }
+    acc[getId(node)] = node.properties.eid;
+    return acc;
+  }, {});
+  // Maps node EIDs to their bosses.
+  const bossMap = rawEdges.reduce((acc, edge) => {
+    acc[nodeMap[getId({identity: edge.start})]] =
+      nodeMap[getId({identity: edge.end})];
+    return acc;
+  }, {});
+
+  const nodes = rawNodes.map(node => ({
+    id: getId(node),
+    eid: node.properties.eid,
+    boss: bossMap[node.properties.eid],
+    root: root,
+    label: node.properties.name,
   }));
-  const edges = rawEdges.map(p => ({
-    from: getId({identity: p.start}),
-    to: getId({identity: p.end}),
+
+  const edges = rawEdges.map(edge => ({
+    from: getId({identity: edge.start}),
+    to: getId({identity: edge.end}),
   }));
   return {nodes, edges};
 }
